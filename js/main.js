0b2d3497582650f5f00dbdb4b2680d80656c6dfd
melissagkma <melissama@Melissas-MacBook-Pro.local>
2016-03-20 22:09:46 -0700

    changed to fade in fade out effect

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('.mainNav a').click(
+/*$('.mainNav a').click(
   function(){
     $('.content-box').addClass('hide')
     $( $(this).attr('href') ).removeClass('hide')
@@ -9,11 +9,24 @@ $('.btn-close').click(
   function(){
     $(this).closest('.content-box').addClass('hide')
   }
-)
+)*/
 
 $('.start').click(
   function(){
     $('.start').hide();
     $('.background').show();
+  }
+)
+
+$('.mainNav a').click(
+  function(){
+    //$('.content-box').fadeOut(500);
+    $( $(this).attr('href') ).fadeIn(750);
+  }
+)
+
+$('.btn-close').click(
+  function(){
+    $(this).closest('.content-box').fadeOut(500);
   }
 )
